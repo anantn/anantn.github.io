94f41af7008f83709473fc7dd668452a3744fc33
Anant Narayanan <anant@kix.in>
2022-02-14 00:16:19 -0800

    Topic filtering for talks & blog via list.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
 function toggleDarkMode() {
   if (document.body.classList.contains("dark")) {
-    document.body.className = document.body.className.replace("dark", "");
+    document.body.classList.remove("dark");
     localStorage.removeItem("theme");
-    //document.documentElement.removeAttribute("dark");
   } else {
-    document.body.className += "dark";
+    document.body.classList.add("dark");
     localStorage.setItem("theme", "dark");
-    //document.documentElement.setAttribute("dark", "");
+  }
+}
+
+function search(name) {
+  var list = new List(name, {
+    valueNames: ["list-item", { data: ["categories"] }],
+  });
+
+  var element = document.getElementById(name);
+  list.on("updated", function () {
+    if (list.searched === true || list.filtered === true) {
+      element.classList.add("filtered");
+    } else {
+      element.classList.remove("filtered");
+    }
+  });
+
+  var buttons = element.querySelectorAll(".pure-button-group > button");
+  for (var button of buttons) {
+    button.addEventListener("click", function (e) {
+      var active = "pure-button-active";
+      if (e.target.classList.contains(active)) {
+        e.target.classList.remove(active);
+        list.filter();
+      } else {
+        for (var button of buttons) {
+          button.classList.remove(active);
+        }
+        e.target.classList.add(active);
+        list.filter(function (item) {
+          var check = item.values().categories;
+          if (check && check.includes(e.target.innerHTML.toLowerCase())) {
+            return true;
+          }
+          return false;
+        });
+      }
+    });
   }
 }
